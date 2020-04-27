8995fcd8f79eab5da0a4d634a84adf1d9c4b5462
The Vinh <nguyenthevinh1997@gmail.com>
2020-04-27 10:12:25 +0700

    first commit

---
+++ shopping-cart/src/components/ProductList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+    <ul>
+      <li :key="product.id" v-for="product in products">{{product.title}} - {{product.price}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+import shop from "@/api/shop";
+export default {
+  data() {
+    console.log("Data ne!!!");
+    return {
+      products: []
+    };
+  },
+  created() {
+    console.log("Created ne!!!");
+    shop.getProducts(products => {
+      this.products = products;
+      console.log("Products ne!!!");
+    });
+  }
+};
+</script>
+<style scoped></style>
