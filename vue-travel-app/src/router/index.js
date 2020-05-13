276fcdf408afbe383a1a7d63e68ad1843d6613ad
The Vinh <nguyenthevinh1997@gmail.com>
2020-05-13 13:41:23 +0700

    Vue Router Nested Routes

--- vue-travel-app/src/router/index.js
+++ vue-travel-app/src/router/index.js
@@ -20,6 +20,17 @@ const routes = [
       import(
         /* webpackChunkName: "DestinationDetails" */ "../views/DestinationDetails.vue"
       ),
+    children: [
+      {
+        path: ":experienceSlug",
+        name: "experienceDetails",
+        props: true,
+        component: () =>
+          import(
+            /* webpackChunkName: "ExperienceDetails" */ "../views/ExperienceDetails.vue"
+          ),
+      },
+    ],
   },
 ];
 
