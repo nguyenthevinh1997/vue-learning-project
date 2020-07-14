b50cf878ba50fda01a8b4123ecab0b31bf6d3a18
The Vinh <nguyenthevinh1997@gmail.com>
2020-07-14 10:22:38 +0700

    add nodemon

--- nodejs/notes-app/app.js
+++ nodejs/notes-app/app.js
@@ -1,7 +1,10 @@
-const validator = require('validator')
-const getNotes = require('./notes.js')
+const chalk = require("chalk");
+const getNotes = require("./notes.js");
 
-const msg = getNotes()
-console.log(msg)
+const msg = getNotes();
+console.log(msg);
 
-console.log(validator.isEmail('andrew@example.com'))
+const greenMsg = chalk.blue.inverse.bold("Success!");
+console.log(greenMsg);
+
+console.log(process.argv[2]);
