0b32e8f998328cdcf215eb602518aa93c97ece97
The Vinh <nguyenthevinh1997@gmail.com>
2020-07-22 10:06:35 +0700

    33. An HTTP Request Challenge

--- nodejs/weather-app/app.js
+++ nodejs/weather-app/app.js
@@ -15,3 +15,12 @@ request({ url: url }, (error, response) => {
       " chance of rain."
   );
 });
+
+const geocodeURL =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidmluaHZpbmgyMjEyMyIsImEiOiJja2N3cnB0b3AwZ3kyMzJxN2Q5aHpzdnl2In0.NMBburgxKMZVWfiRZZZ30g";
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+  const latitude = response.body.features[0].center[1];
+  const longitude = response.body.features[0].center[0];
+  console.log(latitude, longitude);
+});
