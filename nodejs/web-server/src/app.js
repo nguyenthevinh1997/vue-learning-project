7d1845d106e2da2147842722f049528d3cafea04
The Vinh <nguyenthevinh1997@gmail.com>
2020-07-24 09:43:11 +0700

    45. Serving up Static Assets

--- nodejs/web-server/src/app.js
+++ nodejs/web-server/src/app.js
@@ -1,23 +1,13 @@
+const path = require("path");
 const express = require("express");
 
+console.log(__dirname);
+console.log(path.join(__dirname, "../public"));
+
 const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
 
-app.get("", (req, res) => {
-  res.send("<h1>Hello express!</h1>");
-});
-
-app.get("/help", (req, res) => {
-  res.send([
-    {
-      name: "Andrew"
-    },
-    { name: "Sarah" }
-  ]);
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>About</h1>");
-});
+app.use(express.static(publicDirectoryPath));
 
 app.get("/weather", (req, res) => {
   res.send({
