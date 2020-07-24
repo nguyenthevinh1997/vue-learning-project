41c773094bc28f675a37d51b51ff223e9063f0e1
The Vinh <nguyenthevinh1997@gmail.com>
2020-07-24 11:29:39 +0700

    48. Customizing the Views Directory

--- nodejs/web-server/src/app.js
+++ nodejs/web-server/src/app.js
@@ -6,8 +6,10 @@ console.log(path.join(__dirname, "../public"));
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates");
 
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
 app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
