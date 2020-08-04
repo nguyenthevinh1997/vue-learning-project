9542ab4a49a7794916a83211e4644cc65ad9ffcf
The Vinh <nguyenthevinh1997@gmail.com>
2020-08-04 10:40:52 +0700

    55. Building a JSON HTTP Endpoint

--- nodejs/web-server/src/app.js
+++ nodejs/web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
 console.log(__dirname);
 console.log(path.join(__dirname, "../public"));
@@ -42,9 +44,44 @@ app.get("/help", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide address"
+    });
+  }
+  geocode(req.query.address, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return res.send({ error });
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address
+      });
+    });
+  });
+
+  // res.send({
+  //   forecast: "It is snowing",
+  //   location: "Philadelphia",
+  //   address: req.query.address
+  // });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term"
+    });
+  }
+
+  console.log(req.query.search);
   res.send({
-    forecast: "It is snowing",
-    location: "Philadelphia"
+    products: []
   });
 });
 
